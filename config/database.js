25b96cb95fc57228c243960790391ab7aed01284
AHachey17 <ahachey17@gmail.com>
2023-04-30 22:15:01 -0400

    fixed trash

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+    //objects, that allows us to use in other places
+    'url': 'mongodb+srv://ahachey17:ABC123@clientcards.sxdop1q.mongodb.net/clientcards?retryWrites=true&w=majority', 
+    'dbName': 'expressprev'
+};
